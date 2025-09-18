06addda96c53c659b8d5c4aa75b2cc0d6adf7e96
codersantanu <codingwithsantanu308@gmail.com>
2025-09-18 21:13:08 +0530

    Enhance weather fetching functionality with detailed output and error handling

--- weather.py
+++ weather.py
@@ -2,4 +2,20 @@ import requests
 def get_weather(city):
     
     API_KEY="MY_API_KEY"
-    URL=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    URL=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    
+    try:
+        response = requests.get(URL)
+        data=response.json()
+        if response.status_code==200:
+            print(f"Weather in {data['name']},{data['sys']['country']}:")
+            print(f"Temperature: {data['main']['temp']}°C")
+            print(f"Humidity :{data['main']['humidity']}%")
+            print(f"Weather : {data ["Weather"][0]["description"]}")
+            print(f"Wind Speed : {data['wind']['speed']} m/s")
+        else:
+            print(f"City{city} not found. Please check the city name and try again.")
+            
+    except Exception as e:
+        print("Something went wrong. Please try again later.")
+        
